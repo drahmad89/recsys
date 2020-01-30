8bda2cd0a8c490d25f4231a40bb64b34a60541b6
Ahmad Chamseddine <doctor.ahmad89@gmail.com>
2020-01-29 20:15:41 -0500

    updates

--- db_connector.py
+++ db_connector.py
@@ -1,36 +1,51 @@
 #!/usr/bin/env python
 import mysql.connector as mariadb
-import pandas as pd
 import numpy as np
+import pandas as pd
 import pdb
+import os
+import sys
 
-def test_class():
+
+def dataloading(user_column, item_column, feature, file_dir_name,  query = False):
     database_connector = Database_connector('localhost', 'root', 'fadwa123',
                                             'recsys_1')
-    database_connector.query("SELECT CardholderID , MOBI_MCC_ID, F_score, M_score, L_score, P_score FROM carholdermccscorings WHERE SnapshotTag = '20170401-20190401'")
-    database_connector.pivot("CardholderID", "MOBI_MCC_ID", "M_score")
-    database_connector.save_df(dataframe='pivot3.pkl')
-
+    if query:
+        database_connector.query(("SELECT {}, {}, {} 
+                                 FROM carholdermccscorings 
+                                 WHERE SnapshotTag =
+                                 '20170401-20190401'").format(user_column,
+                                                             item_column,
+                                                            feature))
+    database_connector.query(query=None)
+    database_connector.columns_subset(user_column,item_column,feature)
+    database_connector.hashmap_pk(user_column, item_column, query=True)
+    database_connector.pivot(user_column, item_column, feature)
+    database_connector.save_df(dataframe=file_dir_name)
 
 class Database_connector:
 
     def __init__(self, host, user, password, database):
-        self.mariadb_connection = mariadb.connect(host='localhost', user= user,
-                                                  password=password,
-                                                  database='recsys_1')
+        try:
+            self.mariadb_connection = mariadb.connect(host='localhost', user= user,
+                                                      password=password,
+                                                      database='recsys_1')
+        except:
+            print('Connection to database has failed!!')
+            self.mariadb_connection = None
         self.df = None
 
-    def query(self, query=None):
+    def query(self, query=None, direc = None):
         if query:
             self.df =  pd.read_sql(query, con=self.mariadb_connection)
             return self.df
-        self.df = pd.read_pickle('./df_3.pkl')
 
+        self.df = pd.read_pickle(direc, compression=None)
         return self.df
 
-
     def columns_subset(self, *args):
-        return self.df[list(args)]
+        self.df = self.df[list(args)]
+        return
 
     def _hashmap(self, df, column, column_key):
         values = sorted(df[column].unique())
@@ -38,12 +53,11 @@ class Database_connector:
         df[column_key] = df[column].map(d)
         return df
 
-    def hashmap_pk(self, query = None):
+    def hashmap_pk(self,user_column, item_column,  query = None):
         if query:
-            self.df = self._hashmap(self.df,'CardholderID', 'UserKey')
-            self.df = self._hashmap(self.df, 'MOBI_MCC_ID', 'ItemKey')
+            self.df = self._hashmap(self.df,user_column, 'UserKey')
+            self.df = self._hashmap(self.df, item_column, 'ItemKey')
             self.df.sort_values(by=['UserKey', 'ItemKey'], inplace= True)
-
         return self.df
 
     def pivot(self, index, columns, values):
@@ -60,8 +74,11 @@ class Database_connector:
             return False
 
     def __del__(self):
-        print ("Closing mariadb connection session...\n")
-        self.mariadb_connection.close()
+        try:
+            print ("Closing mariadb connection session...\n")
+            self.mariadb_connection.close()
+        except:
+            print("Loading data is done!")
 
 if __name__ == "__main__":
-    test_class()
+    test_class('CardholderID', 'MOBI_MCC_ID', 'M_score')
